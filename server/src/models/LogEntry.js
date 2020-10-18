a3c7686b444b7150ec175e51a43b8285cfab773d
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-17 23:30:11 -0500

    logentry schema modeled and .env set up

---
+++ server/src/models/LogEntry.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const logentrySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  }, // String is shorthand for {type: String}
+  description: String,
+  body: String,
+  comments: String,
+  image: String,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+    default: 0,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+    min: -90,
+    max: 90,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+    min: -180,
+    max: 180,
+  },
+  visitDate: {
+    required: true,
+    type: Date,
+  },
+},
+{
+  timestamps: true,
+});
+
+const LogEntry = mongoose.model('LogEntry', logentrySchema);
+
+module.exports = LogEntry;
