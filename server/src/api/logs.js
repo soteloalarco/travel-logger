4b122bd280cf909fc285e0b5735bdb374e982156
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-18 16:53:15 -0500

    try catch added when GET

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -4,9 +4,13 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  const entries = await LogEntry.find();
-  res.json(entries);
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
