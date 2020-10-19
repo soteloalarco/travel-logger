3f79bdcbca1a86f280e81504ff703f36ae60c954
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-18 23:24:43 -0500

    pins and popups working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 
 import { listLogEntries } from './API';
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -26,22 +27,52 @@ const App = () => {
   return (
     <ReactMapGL
       {...viewport}
-      mapStyle= 'mapbox://styles/soteloalarco/ckgfu81286ioh19pgv8isi8w0'
+      mapStyle= 'mapbox://styles/soteloalarco/ckgg0fi5c097219qlp58r4muj'
       ReactMapGL mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} 
       onViewportChange={nextViewport => setViewport(nextViewport)}
     >
       {
         logEntries.map(entry => (
-          <Marker 
-          key={entry._id}
-          latitude={entry.latitude} 
-          longitude={entry.longitude} 
-          offsetLeft={-20} 
-          offsetTop={-10}>
-          <div>{entry.title}</div>
-          </Marker>
+          <>
+            <Marker 
+              key={entry._id}
+              latitude={entry.latitude} 
+              longitude={entry.longitude} 
+              >
+              
+              <div
+                onClick={()=> setShowPopup({
+                  ...showPopup,
+                  [entry._id]: true,
+                })}
+              >
+                <img 
+                  src="https://i.imgur.com/y0G5YTX.png" 
+                  alt="marker" 
+                  className="marker"
+                  style= {{
+                    height: `${6 * viewport.zoom}px`,
+                    width: `${6 * viewport.zoom}px`,
+                  }}
+                />
+              </div>
+            </Marker>
+            {
+              showPopup[entry._id] ? (
+                <Popup
+                  latitude={entry.latitude} 
+                  longitude={entry.longitude} 
+                  closeButton={true}
+                  closeOnClick={false}
+                  onClose={() => this.setState({showPopup: false})}
+                  anchor="top" >
+                  <div>You are here</div>
+                </Popup>) : null
+            }
+          </>
         ))
       }
+
     </ReactMapGL>
   );
 }
